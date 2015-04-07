07d5b3d64aabf1265568939977e6e091a094a6f5
David Eliason & Kyle Giard-Chase <student@epicodus.com>
2015-04-07 13:43:56 -0700

    Add html to take and display result of user input.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ var titleCase = function(words) {
 
     for(var i = 1; i < word_array.length; i++)
     {
-        if (word_array[i] === 'in' || word_array[i] === 'the' || word_array[i] === 'a' || word_array[i] === "an" || word_array[i] === "at"|| word_array[i] === "by" || word_array[i] ==="for" || word_array[i] === "of" || word_array[i] ==="on" || word_array[i] === "to" || word_array[i] ==="up" || word_array[i] ==="and" || word_array[i] === "as" || word_array[i] === "but" || word_array[i] === "it" || word_array[i] === "or" || word_array[i] === "nor")
+        if (word_array[i] === 'in' || word_array[i] === 'the' || word_array[i] === 'a' || word_array[i] === "an" || word_array[i] === "at"|| word_array[i] === "by" || word_array[i] ==="for" || word_array[i] === "of" || word_array[i] ==="on" || word_array[i] === "to" || word_array[i] ==="up" || word_array[i] ==="and" || word_array[i] === "as" || word_array[i] === "but" || word_array[i] === "it" || word_array[i] === "or" || word_array[i] === "nor" || word_array[i] === 'is')
         {
             title_cased_words.push(word_array[i]);
         }
@@ -23,3 +23,27 @@ var titleCase = function(words) {
     return title_cased_words.join(" ");
 
 };
+
+$(document).ready(function() {
+
+    $('form#titleCaseForm').submit(function(event) {
+        var words = $('input#inputWords').val();
+        console.log(words);
+        var result = titleCase(words);
+
+        $(".title_result").empty();
+
+        $("#result").text(result);
+
+        $("#result").show();
+        event.preventDefault();
+    });
+
+});
+
+/*
+<form id="titleCaseForm">
+    <label for="inputWords">Enter words</label>
+    <input name="inputWords" id="inputWords" type="text" placeholder="words here" class="form-control">
+    <button type="submit" class="btn btn-warning">Submit</button>
+*/
